ddde2da3d0b536edebc035afe37689c01c85d95f
missmagenta <sam126863@gmail.com>
2023-02-26 20:50:11 +0300

    Renamed the boolean variable

--- src/main/moves/Facade.java
+++ src/main/moves/Facade.java
@@ -9,16 +9,16 @@ public class Facade extends PhysicalMove {
     public Facade() {
         super(Type.NORMAL, 70, 100);
     }
-    private boolean flag;
+    private boolean special;
     protected void applyOppDamage(Pokemon def, double damage) {
         Status state = def.getCondition();
-        flag = true;
+        special = true;
         if (state == Status.BURN || state == Status.PARALYZE || state == Status.POISON) {
             def.setMod(Stat.HP, 2 * (int) Math.round(damage));
         }
     }
     protected String describe() {
-        if (flag) return "damages with double power";
+        if (special) return "damages with double power";
         else return "damages";
     }
 }
