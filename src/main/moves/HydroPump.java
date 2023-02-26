6a4e6fbee1a04d769ba143bb9966fae6be917715
missmagenta <sam126863@gmail.com>
2023-02-26 21:16:24 +0300

    Deleted a method that doesn't have to be overridden

--- src/main/moves/HydroPump.java
+++ src/main/moves/HydroPump.java
@@ -5,9 +5,6 @@ public class HydroPump extends SpecialMove {
     public HydroPump(){
         super(Type.WATER, 110, 80);
     }
-    protected void applyOppDamage(Pokemon p, double damage) {
-        p.setMod(Stat.HP, (int)Math.round(damage));
-    }
     @Override
     protected String describe() {
     return "blasts out a huge volume of water at high power";
