a5b5919102221cf92f7f93c0556ec978e1a7fe8c
missmagenta <sam126863@gmail.com>
2023-02-26 21:30:25 +0300

    Levels were determined in accordance with their attacks. Team members were changed

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,19 +7,19 @@ import main.moves.*;
 public class Main {
     public static void main(String[] args) {
         Battle b = new Battle();
-        Braviary p1 = new Braviary("Орлан",1);
-        Deoxys p2 = new Deoxys("Вирус из космоса", 1);
+        Braviary p1 = new Braviary("Орлан",63);
+        Deoxys p2 = new Deoxys("Вирус из космоса", 49);
         Poliwag p3 = new Poliwag("Головастик",1);
-        Poliwhirl p4 = new Poliwhirl("Амфибия", 1);;
-        Poliwrath p6 = new Poliwrath("СверхАмфибия", 1);
-        Rufflet p5 = new Rufflet("Орленок", 1);
+        Poliwhirl p4 = new Poliwhirl("Амфибия", 48);
+        Poliwrath p5 = new Poliwrath("СверхАмфибия", 1);
+        Rufflet p6 = new Rufflet("Орленок", 59);
 
-        b.addAlly(p1);
         b.addAlly(p2);
         b.addAlly(p3);
+        b.addAlly(p6);
+        b.addFoe(p1);
         b.addFoe(p4);
         b.addFoe(p5);
-        b.addFoe(p6);
         b.go();
     }
 }
